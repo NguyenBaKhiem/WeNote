cf4547ae2a79f11895facc2f3d48116a33473199
tansonhn <tansonhn@gmail.com>
2019-12-29 10:54:27 +0700

    update migrations & seeders

--- database/seeds/ReportFlagsTableSeeder.php
+++ database/seeds/ReportFlagsTableSeeder.php
@@ -12,5 +12,15 @@ class ReportFlagsTableSeeder extends Seeder
     public function run()
     {
         DB::table('report_flags')->truncate();
+
+        $data = [
+            ['name' => 'Nội dung hư cấu', 'style' => 'warning'],
+            ['name' => 'Hình ảnh không phù hợp', 'style' => 'warning'],
+            ['name' => 'Nội dung hoặc hình ảnh nhạy cảm', 'style' => 'warning'],
+            ['name' => 'Nội dung hoặc hình ảnh bạo lực', 'style' => 'danger'],
+            ['name' => 'Nội dung liên quan đến chính trị', 'style' => 'danger'],
+        ];
+
+        DB::table('report_flags')->insert($data);
     }
 }
