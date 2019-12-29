4a7a2eedb5ba09aa6d67f8777c0cab3ada5365aa
NguyenBaKhiem <duoinhungconmuak@gmail.com>
2019-12-29 12:19:10 +0700

    update api get notes

--- app/Http/Services/NoteService.php
+++ app/Http/Services/NoteService.php
@@ -15,7 +15,8 @@ class NoteService
 	public function getNotes($input)
 	{
 		return Note::join('note_flags', 'note_flags.id', 'note_flag_id')
-				->get();
+				->join('users', 'users.id', 'created_by')
+				->get(['note_flags.id', 'note_flags.name', 'note_flags.style', 'notes.title', 'notes.description', 'notes.longitude', 'latitude', 'osm_id', 'osm_type', 'osm_key', 'osm_value', 'post_code', 'country', 'city', 'district', 'street', 'house_number', 'created_by', 'notes.created_at']);
 	}
 
 	public function addNote($input)
